e52e3302dfdbd5abb5dfbc268654658d65876909
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-10-31 22:54:56 -0300

    update

--- CursoJS/aula11/ex008.js
+++ CursoJS/aula11/ex008.js
@@ -1,3 +1,9 @@
-var vel = 60.5
+var vel = 78
 
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { // Condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`)
