bd563a82fd3787b44695cb1a0540085783f1d39c
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-06 18:54:19 -0300

    Update ex009.js

--- CursoJS/aula11/ex009.js
+++ CursoJS/aula11/ex009.js
@@ -1,9 +1,14 @@
-var país = 'Brasil'
+// Definindo uma variável 'país' e atribuindo o valor 'Brasil' a ela.
+var país = 'Brasil';
 
-console.log(`Vivendo em ${país}`)
+// Exibindo uma mensagem no console que inclui o valor da variável 'país'.
+console.log(`Vivendo em ${país}`);
 
+// Iniciando uma estrutura condicional 'if' para verificar se a variável 'país' é igual a 'Brasil'.
 if (país == 'Brasil') {
-    console.log('Você é Brasileiro!')
+    // Se a condição for verdadeira, exibe uma mensagem indicando que a pessoa é brasileira.
+    console.log('Você é Brasileiro!');
 } else {
-    console.log('Você é Estrangeiro')
+    // Se a condição for falsa, exibe uma mensagem indicando que a pessoa é estrangeira.
+    console.log('Você é Estrangeiro');
 }
