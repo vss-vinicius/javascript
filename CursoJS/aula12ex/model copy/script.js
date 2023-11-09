056b25c19b99e61d3a7d3247b7f316fd83ac811b
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-09 16:07:55 -0300

    update

---
+++ CursoJS/aula12ex/model copy/script.js
@@ -0,0 +1,9 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    var segundos = data.getSeconds()
+}
