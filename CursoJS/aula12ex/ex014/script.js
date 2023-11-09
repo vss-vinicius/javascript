b33ea0366024c69815d82cc4ecd7f2dd0bca92ec
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-09 16:21:02 -0300

    update

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -6,4 +6,14 @@ function carregar() {
     var hora = data.getHours()
     var minutos = data.getMinutes()
     var segundos = data.getSeconds()
+
+    msg.innerHTML = `Agora são exatamente ${hora}:${minutos}:${segundos}`
+
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tarde!
+    } else {
+        // Boa noite!
+    }
 }
