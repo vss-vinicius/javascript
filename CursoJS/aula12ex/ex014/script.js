182bfe06c82db0112a5d459ed9cfd0b5f652c717
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-09 16:35:30 -0300

    Update script.js

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -2,21 +2,25 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
 
-    var data = new Date()
-    var hora = data.getHours()
-    var minutos = data.getMinutes()
-    var segundos = data.getSeconds()
+    setInterval(function () {
+        var data = new Date()
+        var hora = data.getHours()
+        var minutos = data.getMinutes()
+        var segundos = data.getSeconds()
 
-    msg.innerHTML = `Agora são exatamente ${hora}:${minutos}:${segundos}`
+        msg.innerHTML = `Agora são exatamente ${hora}:${minutos}:${segundos}`
 
-    if (hora >= 0 && hora < 12) {
-        // Bom dia!
-        img.src = 'images/manha.png'
-    } else if (hora >= 12 && hora < 18) {
-        // Boa tarde!
-        img.src = 'images/tarde.png'
-    } else {
-        // Boa noite!
-        img.src = 'images/noite.png'
-    }
+        if (hora >= 0 && hora < 12) {
+            // Bom dia!
+            img.src = 'images/manha.png'
+        } else if (hora >= 12 && hora < 18) {
+            // Boa tarde!
+            img.src = 'images/tarde.png'
+        } else {
+            // Boa noite!
+            img.src = 'images/noite.png'
+        }
+    }, 1000) // Atualizar a cada segundo
 }
+
+carregar() // Chame a função uma vez para configurar a exibição inicial
