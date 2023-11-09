0f0701c79097797044bc597cc0eb6f8c4ee7960e
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-09 16:25:09 -0300

    Update script.js

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -11,9 +11,12 @@ function carregar() {
 
     if (hora >= 0 && hora < 12) {
         // Bom dia!
+        img.src = 'images/manha.png'
     } else if (hora >= 12 && hora < 18) {
         // Boa tarde!
+        img.src = 'images/tarde.png'
     } else {
         // Boa noite!
+        img.src = 'images/noite.png'
     }
 }
