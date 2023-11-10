cfa428fb07a5eb730255440ee93b533c1a4a0cfe
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-10 14:04:52 -0300

    update

--- CursoJS/aula12ex/ex015/script.js
+++ CursoJS/aula12ex/ex015/script.js
@@ -17,27 +17,36 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'images/crianca-m.png')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'images/jovem-m.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'images/adulto-m.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'images/idoso-m.png')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'images/crianca-f.png')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'images/jovem-f.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'images/adulta-f.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'images/idosa-f.png')
             }
         }
 
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
