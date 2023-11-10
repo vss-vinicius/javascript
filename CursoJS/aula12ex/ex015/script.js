76bbf9edd474979683ef9a14f72159e0ea57162a
Vinicius Soares Santos <100010777+vss-vinicius@users.noreply.github.com>
2023-11-10 13:56:46 -0300

    Update script.js

--- CursoJS/aula12ex/ex015/script.js
+++ CursoJS/aula12ex/ex015/script.js
@@ -7,6 +7,37 @@ function verificar() {
     if (fano.value.lenght == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
 }
