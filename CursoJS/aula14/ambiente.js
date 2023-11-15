b964f37e021fa544da2924cad07d8a42310e065a
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2023-11-14 23:22:23 -0300

    Create ambiente.js

---
+++ CursoJS/aula14/ambiente.js
@@ -0,0 +1,10 @@
+/* 
+Este é um loop "for" que inicializa a variável 'c' com o valor 1. 
+O loop continuará enquanto 'c' for menor ou igual a 5. 
+A cada iteração, 'c' é incrementado em 1.
+*/
+
+for (c = 1; c <= 5; c++) {
+    // Esta linha imprime no console uma mensagem que inclui o valor atual de 'c'.
+    console.log(`Número ${c}`);
+}
