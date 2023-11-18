3ecd12f7d8a5691106d186772cbc9c28086252c4
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2023-11-18 16:49:43 -0300

    Update vetornatela2.js

--- CursoJS/aula15/vetornatela2.js
+++ CursoJS/aula15/vetornatela2.js
@@ -1,9 +1,21 @@
-let valores = [8, 1, 7, 4, 2, 9]
+// Cria um array chamado 'valores' com os valores 8, 1, 7, 4, 2, 9.
+let valores = [8, 1, 7, 4, 2, 9];
 
-valores.sort()
+// Ordena o array 'valores' lexicograficamente (como strings), pois 'sort' converte os elementos em strings por padrão.
+valores.sort();
 
-console.log(valores)
+// Imprime no console o array 'valores' após a ordenação.
+console.log(valores);
 
+// Loop for...in que percorre cada posição (ou chave) no array 'valores'.
+// A variável 'pos' representa a posição atual no array.
 for (let pos in valores) {
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    // Imprime no console a mensagem indicando a posição e o valor correspondente.
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
 }
+
+/*
+O método sort é usado sem uma função de comparação, portanto, os elementos são ordenados lexicograficamente como strings. Se desejar uma ordenação numérica correta, é recomendável usar uma função de comparação, conforme mencionado anteriormente.
+
+O loop for...in é usado para percorrer as posições (ou chaves) do array. No entanto, ao usar for...in em arrays, pode haver comportamentos inesperados, especialmente se propriedades adicionais foram adicionadas ao objeto Array.prototype. É mais comum usar o loop for...of para iterar sobre os valores dos arrays.
+*/
